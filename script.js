fdb6dfcec81fc9fd1d3ed9528e7576e2699c8a45
perizaternisova-ui <perizaternisova@icloud.com>
2026-07-25 02:19:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -187,20 +187,20 @@ function createCards(category, containerId){
         card.className = `card ${item.rarity}`;
 
         card.innerHTML = `
-            <div class="emoji">${item.emoji}</div>
+    <div class="emoji">${item.emoji}</div>
 
-            <h3>${item.name}</h3>
+    <h3>${item.title}</h3>
 
-            <p>${item.description}</p>
+    <p>${item.description}</p>
 
-            <div class="price">
-                ❤️ ${item.price}
-            </div>
+    <div class="price">
+        ❤️ ${item.price}
+    </div>
 
-            <button class="buy-btn">
-                Получить ✨
-            </button>
-        `;
+    <button class="buy-btn">
+        Получить ✨
+    </button>
+`;
 
         container.appendChild(card);
 
