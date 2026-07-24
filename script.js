c9235203c6d792add47f4012d1f0969a23acfa8b
perizaternisova-ui <perizaternisova@icloud.com>
2026-07-24 19:25:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 function createHeart() {
 
-const heart = document.createElement("div");
+    const heart = document.createElement("div");
 
-heart.className = "floating-heart";
+    heart.innerHTML = "❤";
 
-const hearts = ["❤️","🩷","💖","💕","💗"];
+    heart.classList.add("love-heart");
 
-heart.innerHTML = hearts[Math.floor(Math.random()*hearts.length)];
+    heart.style.left = Math.random() * 100 + "vw";
 
-heart.style.left = Math.random()*100 + "vw";
+    heart.style.fontSize = (12 + Math.random() * 18) + "px";
 
-heart.style.fontSize = (16 + Math.random()*26) + "px";
+    heart.style.animationDuration = (8 + Math.random() * 6) + "s";
 
-heart.style.animationDuration = (8 + Math.random()*8) + "s";
+    document.body.appendChild(heart);
 
-heart.style.opacity = 0.15 + Math.random()*0.25;
-
-document.body.appendChild(heart);
-
-setTimeout(()=>{
-heart.remove();
-},16000);
+    setTimeout(() => {
+        heart.remove();
+    }, 15000);
 
 }
 
-setInterval(createHeart,900);
-
-это script.js
+setInterval(createHeart, 700);
