36ba65326d4553c52b1f1c152022e53e87f379c9
perizaternisova-ui <perizaternisova@icloud.com>
2026-07-25 11:49:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -217,7 +217,11 @@ const button = card.querySelector(".buy-btn");
 
 button.addEventListener("click", () => {
 
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
     cart.push(item);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
 
     alert(`💖 "${item.title}" добавлен в корзину!`);
 
