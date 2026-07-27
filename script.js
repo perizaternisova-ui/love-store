1cd6040e0d413419cddc5760c3a551324398efc9
perizaternisova-ui <perizaternisova@icloud.com>
2026-07-27 09:21:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const heartsContainer = document.getElementById("hearts");
 
+// Создаём баланс при первом запуске
+if (localStorage.getItem("loveBalance") === null) {
+    localStorage.setItem("loveBalance", "1000");
+}
+
+// Показываем баланс
+const balanceElement = document.getElementById("balance");
+
+if (balanceElement) {
+    balanceElement.textContent = localStorage.getItem("loveBalance");
+}
+
 let cart = [];
 
 function createHeart() {
