6e09757b58a77fe97881f121d912095ef376702a
perizaternisova-ui <perizaternisova@icloud.com>
2026-07-31 14:05:46 +0600

    Update gifts.js

--- gifts.js
+++ gifts.js
@@ -1,1 +1,34 @@
-hello
+// ==============================
+// Love Store
+// Мои подарки
+// ==============================
+
+// Ключ localStorage
+const GIFTS_STORAGE_KEY = "loveGifts";
+
+// Получить все подарки
+function getGifts() {
+    const gifts = localStorage.getItem(GIFTS_STORAGE_KEY);
+
+    return gifts ? JSON.parse(gifts) : [];
+}
+
+// Сохранить подарки
+function saveGifts(gifts) {
+    localStorage.setItem(GIFTS_STORAGE_KEY, JSON.stringify(gifts));
+}
+
+// Загрузить раздел "Мои подарки"
+function loadGifts() {
+
+}
+
+// Добавить подарок
+function addGift(gift) {
+
+}
+
+// Использовать подарок
+function useGift(id) {
+
+}
