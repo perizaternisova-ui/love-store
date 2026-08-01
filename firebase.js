d4a06331e04e8460a19db520ea939856528352e4
perizaternisova-ui <perizaternisova@icloud.com>
2026-08-01 18:12:49 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8R55MCRgCjwi8aH-vLiP3IOCJS_Cnkjw",
-  authDomain: "love-store-app.firebaseapp.com",
-  projectId: "love-store-app",
-  storageBucket: "love-store-app.firebasestorage.app",
-  messagingSenderId: "878435294971",
-  appId: "1:878435294971:web:fb707658a1ee955f0a9a5e"
+
+    apiKey: "AIzaSyC8R55MCRgCjwi8aH-vLiP3IOCJS_Cnkjw",
+
+    authDomain: "love-store-app.firebaseapp.com",
+
+    projectId: "love-store-app",
+
+    storageBucket: "love-store-app.firebasestorage.app",
+
+    messagingSenderId: "878435294971",
+
+    appId: "1:878435294971:web:fb707658a1ee955f0a9a5e"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-export { app };
+const db = getFirestore(app);
+
+export { db };
