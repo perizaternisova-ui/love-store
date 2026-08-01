ea538cd6f2ea3adfba56936c85722d727a05e24f
perizaternisova-ui <perizaternisova@icloud.com>
2026-08-01 19:15:43 +0600

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8R55MCRgCjwi8aH-vLiP3IOCJS_Cnkjw",
+  authDomain: "love-store-app.firebaseapp.com",
+  projectId: "love-store-app",
+  storageBucket: "love-store-app.firebasestorage.app",
+  messagingSenderId: "878435294971",
+  appId: "1:878435294971:web:fb707658a1ee955f0a9a5e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
