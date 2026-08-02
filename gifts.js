30dee27e111065bbd59d51ab4f02f774c85f7465
perizaternisova-ui <perizaternisova@icloud.com>
2026-08-02 14:50:59 +0600

    Update gifts.js

--- gifts.js
+++ gifts.js
@@ -6,19 +6,7 @@
 // Ключ localStorage
 const GIFTS_STORAGE_KEY = "loveGifts";
 
-// Получить все подарки
-function getGifts() {
-    const gifts = localStorage.getItem(GIFTS_STORAGE_KEY);
 
-    return gifts ? JSON.parse(gifts) : [];
-}
-
-// Сохранить подарки
-function saveGifts(gifts) {
-    localStorage.setItem(GIFTS_STORAGE_KEY, JSON.stringify(gifts));
-}
-
-// Загрузить раздел "Мои подарки"
 // Загрузить раздел "Мои подарки"
 function loadGifts() {
 
